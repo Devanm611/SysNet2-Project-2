32fd0dfd03ac1a8737a4a99ad803dd6d5c88ee25
Devanm611 <dmr76@students.uwf.edu>
2025-02-14 23:27:27 -0600

    Created Server File

---
+++ client.cpp
@@ -0,0 +1,43 @@
+/*
+ * Project2 - HTTP Client
+ * COP4635 System & Networks 2
+ * Team Members: Devan Rivera, Ashley Villegas
+ */
+
+ #include <iostream>
+ #include <fstream>
+ #include <sstream>
+ #include <string>
+ #include <cstring>
+ #include <thread> 
+ #include <netinet/in.h> //Socket address structures
+ #include <unistd.h> //System calls (read, write, close)
+ #include <sys/socket.h> //Socket functions
+ #include <arpa/inet.h> //IP address function
+
+int main(){
+
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
