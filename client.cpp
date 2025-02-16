3fac80053eada70f9552851bb69a641288bee06a
BlueMadness27 <ashley654v@gmail.com>
2025-02-16 13:24:14 -0600

    binary to a saved file

--- client.cpp
+++ client.cpp
@@ -4,91 +4,163 @@
  * Team Members: Devan Rivera, Ashley Villegas
  */
 
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <cstring>
-#include <cstdlib>
-#include <unistd.h> // System calls: close
-#include <arpa/inet.h> // IP address functions
-#include <sys/socket.h> // Socket functions
-
-using namespace std;
-
-#define BUFFER_SIZE 4096
-
-int main(int argc, char* argv[]) { 
-
-    if (argc < 3) {// Check if the user provided the server IP and port
-        cerr << "Usage: " << argv[0] << " <Server IP> <Server Port>" << endl;
-        return 1;
-    }
-
-    string serverIP = argv[1];
-    int serverPort = atoi(argv[2]);
-
-    cout << "Connecting to server at IP: " << serverIP << " on port: " << serverPort << endl;
-
-    while (true) {
-
-        // Create a new socket for each request
-        int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-        if (clientSocket < 0) {
-           
-            perror("Socket creation failed");
-            exit(EXIT_FAILURE);
-        }
-
-        // Set server address
-        struct sockaddr_in serverAddr;
-        serverAddr.sin_family = AF_INET;
-        serverAddr.sin_port = htons(serverPort);
-
-        if (inet_pton(AF_INET, serverIP.c_str(), &serverAddr.sin_addr) <= 0) {// Convert IP address to binary form
-            
-            cerr << "Invalid IP Address" << endl;
-            close(clientSocket);
-            return 1;
-        }
-
-        if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) { // Connect to the server
-            
-            perror("Connection failed");
-            close(clientSocket);
-            return 1;
-        }
-
-        cout << "Connected to the server!" << endl;
-
-        string fileName;
-        cout << "Enter the filename to request (or type 'exit' to quit): ";
-        getline(cin, fileName);
-
-        if (fileName == "exit") { 
-            cout << "Exiting client..." << endl;
-            close(clientSocket);
-            break; // Exit the loop when user types 'exit'
-        }
-
-        // Send the request to the server
-        string request = "GET /" + fileName + " HTTP/1.1\r\nHost: " + serverIP + ":" + to_string(serverPort) + "\r\nConnection: close\r\n\r\n";
-        send(clientSocket, request.c_str(), request.size(), 0);
-
-        // Receive the response from the server
-        char buffer[BUFFER_SIZE];
-        int bytesReceived;
-        cout << "\n--- Response from Server ---\n";
-
-        while ((bytesReceived = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0)) > 0) { // Receive data until there is no more data to read
-            buffer[bytesReceived] = '\0';
-            cout << buffer;
-        }
-        cout << "\n--- End of Response ---\n";
-
-        // Close the connection before making another request
-        close(clientSocket);
-    }
-
-    return 0;
-}
+ #include <iostream>
+ #include <fstream>
+ #include <sstream>
+ #include <string>
+ #include <cstring>
+ #include <cstdlib>
+ #include <unistd.h>       // System calls: close()
+ #include <arpa/inet.h>    // IP address functions
+ #include <sys/socket.h>   // Socket functions
+ 
+ using namespace std;
+ 
+ #define BUFFER_SIZE 4096  // Buffer size for reading data from the server
+ 
+ // Function to save binary file (for images)
+ void saveBinaryFile(const string &filename, const char *data, size_t size) {
+     ofstream file(filename, ios::binary);
+     if (!file) {
+         cerr << "[ERROR] Could not save file: " << filename << endl;
+         return;
+     }
+     file.write(data, size);
+     file.close();
+     cout << "[INFO] Image saved as: " << filename << endl;
+ }
+ 
+ // Function to handle and process HTTP responses
+ void processHttpResponse(int clientSocket) {
+     char buffer[BUFFER_SIZE];
+     string response;
+     int bytesReceived;
+     bool isImage = false;
+     string contentType;
+     size_t contentLength = 0;
+     ofstream imageFile;
+ 
+     // Read response from the server
+     while ((bytesReceived = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0)) > 0) {
+         buffer[bytesReceived] = '\0';
+         response += buffer;
+ 
+         // Look for end of HTTP headers
+         size_t headerEnd = response.find("\r\n\r\n");
+         if (headerEnd != string::npos) {
+             string headers = response.substr(0, headerEnd);
+             cout << "\n--- HTTP Headers ---\n" << headers << "\n";
+ 
+             // Detect if response is an image
+             if (headers.find("Content-Type: image") != string::npos) {
+                 isImage = true;
+ 
+                 // Extract Content-Length (if present)
+                 size_t lengthPos = headers.find("Content-Length: ");
+                 if (lengthPos != string::npos) {
+                     stringstream ss(headers.substr(lengthPos + 16));
+                     ss >> contentLength;
+                 }
+ 
+                 // Extract content type
+                 size_t typePos = headers.find("Content-Type: ");
+                 if (typePos != string::npos) {
+                     contentType = headers.substr(typePos + 14);
+                     contentType = contentType.substr(0, contentType.find("\r"));
+                 }
+ 
+                 // Save image data
+                 string filename = "downloaded_image";
+                 if (contentType.find("jpeg") != string::npos) filename += ".jpg";
+                 else if (contentType.find("png") != string::npos) filename += ".png";
+ 
+                 imageFile.open(filename, ios::binary);
+                 if (!imageFile) {
+                     cerr << "Could not create file: " << filename << endl;
+                     return;
+                 }
+ 
+                 // Write remaining data after headers
+                 imageFile.write(buffer + headerEnd + 4, bytesReceived - headerEnd - 4);
+             } else {
+                 // If not an image, print the response body
+                 cout << "\n--- HTTP Body ---\n" << response.substr(headerEnd + 4) << "\n";
+                 return;
+             }
+             break;
+         }
+     }
+ 
+     // If it's an image, continue receiving the rest of the binary data
+     if (isImage) {
+         while ((bytesReceived = recv(clientSocket, buffer, BUFFER_SIZE, 0)) > 0) {
+             imageFile.write(buffer, bytesReceived);
+         }
+         imageFile.close();
+         cout << "Image file saved successfully." << endl;
+     }
+ }
+ 
+ int main(int argc, char* argv[]) { 
+     if (argc < 3) {
+         cerr << "Usage: " << argv[0] << " <Server IP> <Server Port>" << endl;
+         return 1;
+     }
+ 
+     string serverIP = argv[1];
+     int serverPort = atoi(argv[2]);
+ 
+     cout << "Connecting to server at IP: " << serverIP << " on port: " << serverPort << endl;
+ 
+     while (true) {
+         // Create socket
+         int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+         if (clientSocket < 0) {
+             perror("Socket creation failed");
+             exit(EXIT_FAILURE);
+         }
+ 
+         // Set server address
+         struct sockaddr_in serverAddr;
+         serverAddr.sin_family = AF_INET;
+         serverAddr.sin_port = htons(serverPort);
+ 
+         if (inet_pton(AF_INET, serverIP.c_str(), &serverAddr.sin_addr) <= 0) {
+             cerr << "Invalid IP Address" << endl;
+             close(clientSocket);
+             return 1;
+         }
+ 
+         // Connect to the server
+         if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+             perror("Connection failed");
+             close(clientSocket);
+             return 1;
+         }
+ 
+         cout << "Connected to the server!" << endl;
+ 
+         // Prompt user for file request
+         string fileName;
+         cout << "Enter the filename to request (or type 'exit' to quit): ";
+         getline(cin, fileName);
+ 
+         if (fileName == "exit") { 
+             cout << "Exiting client..." << endl;
+             close(clientSocket);
+             break;  // Exit the loop
+         }
+ 
+         // Construct the GET request
+         string request = "GET /" + fileName + " HTTP/1.1\r\nHost: " + serverIP + ":" + to_string(serverPort) + "\r\nConnection: close\r\n\r\n";
+         send(clientSocket, request.c_str(), request.size(), 0);
+ 
+         // Process server response
+         processHttpResponse(clientSocket);
+ 
+         // Close the connection before making another request
+         close(clientSocket);
+     }
+ 
+     return 0;
+ }
--- server.cpp
+++ server.cpp
@@ -1,5 +1,5 @@
 /*
- * Project 2 - HTTP Client
+ * Project 2 - HTTP Server
  * COP4635 System & Networks 2
  * Team Members: Devan Rivera, Ashley Villegas
  */
@@ -14,26 +14,24 @@
  #include <unistd.h>     // System calls: read, write, close
  #include <sys/socket.h> // Socket functions
  #include <arpa/inet.h>  // IP address functions
- #include <netdb.h>      // Hostname resolution functions
+ #include <netdb.h>  // Required for `gethostbyname()`
  
  using namespace std;
  
- #define BUFFER_SIZE 4096
+ #define BUFFER_SIZE 4096 // Buffer size for reading files and data
  
- // Function to get the server's IP address
+ // Function to retrieve the server's IP address
  string getServerIPAddress() {
-
-    // Get the hostname
      char hostbuffer[256];
      struct hostent *host_entry;
  
-     // Retrieve the hostname
+     // Get the hostname of the system
      if (gethostname(hostbuffer, sizeof(hostbuffer)) == -1) {
          perror("gethostname");
          return "Unknown";
      }
  
-     // Get the host information
+     // Get host information
      host_entry = gethostbyname(hostbuffer);
      if (!host_entry) {
          perror("gethostbyname");
@@ -43,109 +41,115 @@
      return inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
  }
  
- // Function to construct an HTTP response
- string getHTTPResponse(const string &status, const string &contentType, const string &body) {
-     ostringstream HTTPResponse;
-     HTTPResponse << "HTTP/1.1 " << status << "\r\n";
-     HTTPResponse << "Content-Type: " << contentType << "\r\n";
-     HTTPResponse << "Content-Length: " << body.size() << "\r\n";
-     HTTPResponse << "Connection: close\r\n\r\n";
-     HTTPResponse << body;
-     return HTTPResponse.str();
- }
- 
  // Function to determine Content-Type based on file extension
  string getContentHTTP(const string &filePath) {
-     if (filePath.rfind(".html") != string::npos && filePath.rfind(".html") == filePath.size() - 5) return "text/html";
-     
-     if (filePath.rfind(".jpg") != string::npos && filePath.rfind(".jpg") == filePath.size() - 4) return "image/jpeg";
-     
-     if (filePath.rfind(".jpeg") != string::npos && filePath.rfind(".jpeg") == filePath.size() - 5) return "image/jpeg";
-    
-     if (filePath.rfind(".png") != string::npos && filePath.rfind(".png") == filePath.size() - 4) return "image/png";
-     
-     return "text/plain";
+    if (filePath.rfind(".html") == filePath.size() - 5) return "text/html";
+    if (filePath.rfind(".jpg") == filePath.size() - 4 || filePath.rfind(".jpeg") == filePath.size() - 5) return "image/jpeg";
+    if (filePath.rfind(".png") == filePath.size() - 4) return "image/png";
+    return "text/plain"; // Default type for unknown files
+}
+ 
+ // Function to send HTTP headers
+ void sendHTTPHeader(int clientSocket, const string &status, const string &contentType, size_t contentLength) {
+     ostringstream response;
+     response << "HTTP/1.1 " << status << "\r\n";
+     response << "Content-Type: " << contentType << "\r\n";
+     response << "Content-Length: " << contentLength << "\r\n";
+     response << "Connection: close\r\n\r\n";
+ 
+     string headerStr = response.str();
+     send(clientSocket, headerStr.c_str(), headerStr.size(), 0);
  }
  
- // Function to read file content
- string getFileContent(const string &filePath) {
-     ifstream file(filePath, ios::in | ios::binary);
-     if (file) {
-         ostringstream content;
-         content << file.rdbuf();
-         file.close();
-         return content.str();
+ // Function to serve a file to the client (handles both text and binary data)
+ void serveFile(int clientSocket, const string &filePath) {
+     ifstream file(filePath, ios::in | ios::binary | ios::ate);
+ 
+     // Check if file exists
+     if (!file) {
+         string errorResponse = "<h1>404 Not Found</h1>";
+         sendHTTPHeader(clientSocket, "404 Not Found", "text/html", errorResponse.size());
+         send(clientSocket, errorResponse.c_str(), errorResponse.size(), 0);
+         cout << "[INFO] Response: 404 Not Found" << endl;
+         return;
      }
-     return "";
+ 
+     // Get file size and content type
+     size_t fileSize = file.tellg();
+     file.seekg(0, ios::beg);
+     string contentType = getContentHTTP(filePath);
+ 
+     // Send HTTP headers
+     sendHTTPHeader(clientSocket, "200 OK", contentType, fileSize);
+     cout << "[INFO] Response: 200 OK (" << contentType << ")" << endl;
+ 
+     // Send file content in chunks
+     char buffer[BUFFER_SIZE];
+     while (file.read(buffer, sizeof(buffer))) {
+         send(clientSocket, buffer, file.gcount(), 0);
+     }
+     send(clientSocket, buffer, file.gcount(), 0); // Send remaining bytes
+ 
+     file.close();
  }
  
- // Function to handle client requests (Verbose)
+ // Function to handle incoming client requests
  void handleClient(int clientSocket) {
      char buffer[BUFFER_SIZE];
      int bytesRead = read(clientSocket, buffer, BUFFER_SIZE - 1);
-     
-     // Check if the request is valid
+ 
+     // If reading fails, close the connection
      if (bytesRead <= 0) {
          cerr << "[ERROR] Failed to read request or client disconnected." << endl;
          close(clientSocket);
          return;
      }
  
-     
-     buffer[bytesRead] = '\0'; // Null-terminate the request data
+     // Null-terminate the request buffer
+     buffer[bytesRead] = '\0';
      cout << "\n[INFO] Received Request:\n" << buffer << endl;
  
      istringstream request(buffer);
      string method, path, protocol;
      request >> method >> path >> protocol;
  
-     // Check if the request is valid
+     // Default to "index.html" if root path is requested
      if (path == "/") path = "index.html";
-     else path = path.substr(1);
+     else path = path.substr(1); // Remove leading "/"
  
      cout << "[INFO] Client requested: " << path << endl;
  
-     string response;
+     // Only handle GET requests
      if (method == "GET") {
-         string fileContent = getFileContent(path);
-         if (!fileContent.empty()) { // File found
-
-             response = getHTTPResponse("200 OK", getContentHTTP(path), fileContent);
-             cout << "[INFO] Response: 200 OK (" << getContentHTTP(path) << ")" << endl;
-         } else { // File not found
-
-             response = getHTTPResponse("404 Not Found", "text/html", "<h1>404 Not Found</h1>");
-             cout << "[INFO] Response: 404 Not Found" << endl;
-         }
-     } else { // Invalid request method
-
-         response = getHTTPResponse("400 Bad Request", "text/html", "<h1>400 Bad Request</h1>");
+         serveFile(clientSocket, path);
+     } else {
+         string errorResponse = "<h1>400 Bad Request</h1>";
+         sendHTTPHeader(clientSocket, "400 Bad Request", "text/html", errorResponse.size());
+         send(clientSocket, errorResponse.c_str(), errorResponse.size(), 0);
          cout << "[INFO] Response: 400 Bad Request" << endl;
      }
  
-     // Send the response back to the client
-     write(clientSocket, response.c_str(), response.size());
+     // Close client connection
      close(clientSocket);
      cout << "[INFO] Connection closed with client." << endl;
  }
  
- // Function to generate a random port
+ // Function to generate a random port number
  int generatePort() {
      srand(time(NULL));
-     return 60001 + (rand() % 99);
+     return 60001 + (rand() % 99); // Generates a random port between 60001 - 60099
  }
  
- // Main function
+ // Main function to start the server
  int main() {
-    
-    // Create a socket
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+     // Create the server socket
+     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
      if (serverSocket < 0) {
          perror("Socket creation failed");
          exit(EXIT_FAILURE);
      }
  
-     // Set server address
+     // Set up the server address structure
      struct sockaddr_in serverAddr;
      memset(&serverAddr, 0, sizeof(serverAddr));
      serverAddr.sin_family = AF_INET;
@@ -153,28 +157,28 @@
      int port = generatePort();
      serverAddr.sin_port = htons(port);
  
-     // Bind the socket to the server address
+     // Bind the socket to the address and port
      if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
          perror("Bind failed");
          close(serverSocket);
          exit(EXIT_FAILURE);
      }
  
-     // Listen for incoming connections
+     // Listen for incoming client connections
      if (listen(serverSocket, 10) < 0) {
          perror("Listen failed");
          close(serverSocket);
          exit(EXIT_FAILURE);
      }
  
-     // Print server information
+     // Display server startup information
      string ipAddress = getServerIPAddress();
      cout << "[INFO] Server running on IP: " << ipAddress << endl;
      cout << "[INFO] Server running on port: " << port << endl;
      cout << "[INFO] Access the server at: http://" << ipAddress << ":" << port << endl;
      cout << "[INFO] Waiting for client connections...\n" << endl;
  
-     // Accept client connections
+     // Infinite loop to accept client connections
      while (true) {
          struct sockaddr_in clientAddr;
          socklen_t clientLen = sizeof(clientAddr);
@@ -184,12 +188,13 @@
              continue;
          }
  
-         // Handle client requests in a separate thread
+         // Log client connection and handle in a new thread
          cout << "[INFO] Client connected from: " << inet_ntoa(clientAddr.sin_addr) << endl;
          thread t(handleClient, clientSocket);
          t.detach();
      }
  
+     // Close the server socket (never actually reached in infinite loop)
      close(serverSocket);
      return 0;
  }
